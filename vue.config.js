f9f78a27a3dca4e009c51d328cbfb8c8b6f8b4ce
JoshCunninhHum <jkdimpas@gmail.com>
2023-09-18 05:45:54 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: path.resolve(__dirname, "./docs"),
+  publicPath: '',
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
